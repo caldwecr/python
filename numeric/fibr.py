eb8d317917c024e6325093427d91978e10237312
Courtland Caldwell <courtland@mattermark.com>
2017-01-16 23:30:37 +0000

    Add iterative impl

--- numeric/fibr.py
+++ numeric/fibr.py
@@ -6,6 +6,17 @@ def fib_rec(n, curr = 1, prev = 0):
     else:
         return [curr] + fib_rec(n - 1, curr + prev, curr)
 
+def fib(n):
+    i, prev, curr = 0, 0, 1
+    result = []
+    while i < n:
+        i += 1
+        result.append(curr)
+        next = curr + prev
+        prev = curr
+        curr = next
+    return result
+
 if __name__ == "__main__":
     import sys
     print(fib_rec(int(sys.argv[1])))
