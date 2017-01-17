a003a8cc9bfce8ae8487e377ffff68e5beabcd25
Courtland Caldwell <courtland@mattermark.com>
2017-01-17 04:42:35 +0000

    Use safe lookup instead of inspecting hash

--- challenges/making_anagrams.py
+++ challenges/making_anagrams.py
@@ -2,9 +2,9 @@ def number_needed(a, b):
     """ Returns the number of characters you must delete to make the two strings anagrams of each other """
     letters = {}
     for l in a:
-        letters[l] = letters[l] + 1 if l in letters and letters[l] else 1
+        letters[l] = letters.get(l, 0) + 1
 
     for l in b:
-        letters[l] = letters[l] - 1 if l in letters and letters[l] else -1
+        letters[l] = letters.get(l, 0) - 1
 
     return sum([abs(x) for x in letters.values()])
