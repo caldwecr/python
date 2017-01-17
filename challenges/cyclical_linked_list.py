75db50b906be507f529ceac6372559a67152cd30
Courtland Caldwell <courtland@mattermark.com>
2017-01-17 05:57:15 +0000

    Implement canonical linked list cycle checker

---
+++ challenges/cyclical_linked_list.py
@@ -0,0 +1,21 @@
+def has_cycle(head):
+    if is_terminated_soon(head):
+        return False
+    else:
+        return is_cyclical(head, head.next.next)
+
+def is_terminated_soon(head):
+    if head.next is None:
+        return True
+    elif head.next.next is None:
+        return True
+    else:
+        return False
+
+def is_cyclical(hop, hophop):
+    if hop == hophop:
+        return True
+    elif is_terminated_soon(hophop):
+        return False
+    else:
+        return is_cyclical(hop.next, hophop.next.next)
