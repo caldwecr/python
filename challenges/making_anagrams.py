940931d93afaac550b65c0cc7745f3295f292356
Courtland Caldwell <courtland@mattermark.com>
2017-01-17 00:58:24 +0000

    Add python doc string

--- challenges/making_anagrams.py
+++ challenges/making_anagrams.py
@@ -1,6 +1,7 @@
 from functools import reduce
 
 def number_needed(a, b):
+    """ Returns the number of characters you must delete to make the two strings anagrams of each other """
     letters = {}
     for l in a:
         letters[l] = letters[l] + 1 if l in letters and letters[l] else 1
