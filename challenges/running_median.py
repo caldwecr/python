6a2732f9a4ec75df2aa41f31b08a9ff71a8d8ab2
Courtland Caldwell <courtland@mattermark.com>
2017-01-23 03:24:04 +0000

    Use two balanced heaps that pivot on the median

--- challenges/running_median.py
+++ challenges/running_median.py
@@ -1,19 +1,62 @@
 import heapq
 class RunningMedianList:
     def __init__(self):
-        self.list = []
+        # Store the current median here
+        self.med = None
+        self.left = []
+        self.right = []
+        # diff > 0 means the structure is heavy to the right
+        # diff < 0 means the structure is heavy to the left
+        self.diff = 0
 
     def append(self, item):
-        heapq.heappush(self.list, item)
+        if not self.med:
+            self.med = item
+        elif item > self.med:
+            heapq.heappush(self.right, item)
+            self.diff += 1
+        else:
+            heapq.heappush(self.left, -item)
+            self.diff -= 1
 
+        self.balance()
+
+    def balance(self):
+        # print('balancing start: med, diff, left, right', self.med, self.diff, self.left, self.right)
+        if self.diff > 1:
+            # We are right heavy
+            heapq.heappush(self.left, -self.med)
+            self.med = heapq.heappop(self.right)
+            self.diff -= 2
+            return self.balance()
+        elif self.diff < -1:
+            # We are left heavy
+            heapq.heappush(self.right, self.med)
+            self.med = (-heapq.heappop(self.left))
+            self.diff += 2
+            return self.balance()
+        else:
+            return
+
+    def count_of_items(self):
+        if not self.med:
+            return 0
+        else:
+            return 1 + len(self.left) + len(self.right)
 
     def median(self):
-        n = len(self.list)//2 + 1
-        nl = heapq.nlargest(n, self.list)
+        # print('median: med, diff, left, right', self.med, self.diff, self.left, self.right)
+        i = self.count_of_items()
+        if i == 0:
+            raise ValueError('Empty heap')
+        elif i % 2 == 1:
+            return self.med
+        elif i % 2 == 0:
+            # When i is 2 one side has one node, the other side is empty
+            if self.diff == 1:
+                return (self.med + self.right[0]) / 2
+            else:
+                return (self.med - self.left[0]) / 2
 
-        if len(self.list) % 2 == 1:
-            return nl[-1] / 1
-        else:
-            return (nl[-1] + nl[-2])/2
 
 
--- spec/challenges/running_median_spec.py
+++ spec/challenges/running_median_spec.py
@@ -5,13 +5,6 @@ with description('RunningMedianList'):
     with before.each:
         self.list = running_median.RunningMedianList()
 
-    with description('#append'):
-        with it('inserts the item into the list in sorted order'):
-            self.list.append(2)
-            self.list.append(3)
-            self.list.append(1)
-            # expect(self.list.list).to(equal([1, 2, 3]))
-
     with description('#median'):
         with context('when the list length is odd'):
             with it('returns the value of the middle item as a float'):
